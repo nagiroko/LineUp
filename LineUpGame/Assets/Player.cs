d744e69c37d5bd0a73ec9b8491aa52825fc576cf
nagiroko <hoobrajrohit@yahoo.com>
2023-03-11 17:55:56 -0500

    Raycast
    Objects name show up when clicked

--- LineUpGame/Assets/Player.cs
+++ LineUpGame/Assets/Player.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Camera cam;
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {      
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2d = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2d, Vector2.zero);
+            if(hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
     }
+
+    void Check()
+    {
+      
+
+    }
+
 }
